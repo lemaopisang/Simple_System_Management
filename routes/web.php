24a348d4a38037c6cc770182c8af67a1c8e5447a
lemaopisang <arkandi.2020@gmail.com>
2024-12-06 13:43:56 +0700

    updates

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TaskController;
 
+Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+
 Route::resource('tasks', TaskController::class);
 
-Route::get('/', function () {
-    return view('welcome');
-});
